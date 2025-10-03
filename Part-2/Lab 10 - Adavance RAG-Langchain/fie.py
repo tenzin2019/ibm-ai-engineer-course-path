b25b77d640ff03d61cec1fc3a09bbe5041dde8ec
Wine Coder <47060787+tenzin2019@users.noreply.github.com>
2025-10-04 08:18:37 +1000

    file changed

---
+++ Part-2/Lab 10 - Adavance RAG-Langchain/fie.py
@@ -0,0 +1,10 @@
+import ibm_watsonx_ai
+
+client = ibm_watsonx_ai.Client(
+    project_id="your_project_id",
+    api_key="your_api_key"
+)
+
+client.list_models()
+
+client.get_model(model_id="your_model_id")
